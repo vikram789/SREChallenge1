8386e657b8a51c49badd1adaeb28d35c1a34427d
EC2 Default User <vikram.s.jbs@gmail.com>
2017-10-22 22:45:39 +0000

    final changes

--- pipeline/create-base-ami.groovy
+++ pipeline/create-base-ami.groovy
@@ -2,16 +2,13 @@ node('ansible') {
     stage('checkout code') {
         git url: 'https://github.com/vikram789/SREChallenge1.git'
     }
-    stage('Create-Component-AMI') {
+    stage('Create-Base-AMI') {
         try{
-            sh ". /home/ec2-user/.bash_profile; ansible-playbook -v ansible/component_ami_setup.yml"
+            sh ". /home/ec2-user/.bash_profile; ansible-playbook -v ansible/base_ami_setup.yml"
         }
         catch (exc){
             echo "Something failed..!!"
             currentBuild.result = 'FAILURE'
         } 
-        finally {
-            echo "Deployment may have failed" 
-                 }
     }
 }
